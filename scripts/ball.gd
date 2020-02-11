c91c2a9c645c2fbed9e1d02380e86ee210bcd634
Leirda01 <adrieldj@orange.fr>
2020-02-11 10:05:54 +0100

    Attache le script à l’objet plutôt qu’à l’instance.

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,7 +1,10 @@
 extends RigidBody2D
 
-export var max_speed = 500
+export var max_speed = 200
+
+func _ready():
+	set_axis_velocity(Vector2(100, 100))
 
 func _integrate_forces(_state):
 	if linear_velocity.length() > max_speed:
-		set_axis_velocity(linear_velocity.normalized() * max_speed)
+		linear_velocity = linear_velocity.normalized() * max_speed
