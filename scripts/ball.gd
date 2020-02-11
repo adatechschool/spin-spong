e3e12f3c978c39d4706ef14009117f2361912de0
Leirda01 <adrieldj@orange.fr>
2020-02-11 10:58:11 +0100

    Set a default damping value in project settings.

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,9 +1,9 @@
 extends RigidBody2D
 
-export var max_speed = 200
+export var max_speed = 300
 
 func _ready():
-	set_axis_velocity(Vector2(100, 100))
+	set_axis_velocity(max_speed * Vector2.LEFT / 3)
 
 func _integrate_forces(_state):
 	if linear_velocity.length() > max_speed:
