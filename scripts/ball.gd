37fcdcc28e255f2ca5eebadccb20c848924e795b
Leirda01 <adrieldj@orange.fr>
2020-02-11 01:34:27 +0100

    Export and set ball max_speed value.

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,4 +1,7 @@
 extends RigidBody2D
 
-func _process(delta):
-	pass
+export var max_speed = 500
+
+func _integrate_forces(_state):
+	if linear_velocity.length() > max_speed:
+		set_axis_velocity(linear_velocity.normalized() * max_speed)
