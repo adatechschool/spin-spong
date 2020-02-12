fa4545bf56a93bad81e7819095aaec2aae80dfda
Leirda01 <adrieldj@orange.fr>
2020-02-12 12:09:17 +0100

    Export input map in paddle, create new paddle

--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -1,12 +1,15 @@
 extends RigidBody2D
 
+export var up = ""
+export var down = ""
+
 var rotation_speed = 9000
 var speed = 2000
 var input = 0
 
-func _integrate_forces(state):
-	input = int(Input.is_action_pressed("adriel_goes_down")) - \
-			int(Input.is_action_pressed("adriel_goes_up"))
+func _integrate_forces(_state):
+	input = int(Input.is_action_pressed(down)) - int(Input.is_action_pressed(up))
+
 	if input:
 		applied_force = Vector2(0, input * speed)
 		applied_torque = input * rotation_speed
