b864d91652ecc4540379bfaf25b78af62b54e7cb
Leirda01 <adrieldj@orange.fr>
2020-03-26 18:10:21 +0100

    add second paddle

--- scripts/Lock.gd
+++ scripts/Lock.gd
@@ -1,14 +1,16 @@
 extends RigidBody2D
 
-export var speed = 2000
-export var rotation_speed = 9000
+export var speed := 2000
+export var rotation_speed := 9000
+export var up: String
+export var down: String
 
 var input := 0
 
 onready var paddle := $Paddle as PhysicsBody2D
 
 func _integrate_forces(_state):
-	input = int(Input.is_action_pressed("adriel_goes_down")) - \
-			int(Input.is_action_pressed("adriel_goes_up"))
+	input = int(Input.is_action_pressed(down)) - \
+			int(Input.is_action_pressed(up))
 	applied_force = Vector2(0, input * speed)
 	paddle.applied_torque = input * rotation_speed
