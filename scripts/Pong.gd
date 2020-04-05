d59e8ee2d8537becc51ca6edb3f887bbcbb8849d
Leirda01 <adrieldj@orange.fr>
2020-04-05 18:35:00 +0200

    Improve readability in Score.gd

--- scripts/Pong.gd
+++ scripts/Pong.gd
@@ -3,8 +3,10 @@ extends Node
 func _ready():
 	if $ScoreHandlers/ScoreLuc.connect("body_entered", self, "adriel_touched"):
 		printerr("unable to connect 'body_entered' from 'ScoreLuc'")
+
 	if $ScoreHandlers/ScoreAdriel.connect("body_entered", self, "luc_touched"):
 		printerr("unable to connect 'body_entered' from 'ScoreAdriel'")
+
 	if $ScoreHandlers/ScoreLock.connect("body_entered", self, "reset_lock"):
 		printerr("unable to connect 'body_entered' from 'ScoreLock'")
 
