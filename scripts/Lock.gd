4a4e15ec7ff6779dde7ff093f253d3511395b5f1
Leirda01 <adrieldj@orange.fr>
2020-04-15 18:28:04 +0200

    Allow to change paddle rotation direction

--- scripts/Lock.gd
+++ scripts/Lock.gd
@@ -1,21 +1,23 @@
 extends RigidBody2D
 
-export var speed := 2000
-export var rotation_speed := 9000
+enum RotationStyle {NORMAL = -1, INVERTED = 1}
+export(RotationStyle) var rotation_direction
+
 export(String) var up
 export(String) var down
-
 export(Color) var color
 
-var input := 0
+const speed := 2000
+const rotation_speed := 9000
 
 onready var paddle := $Paddle as PhysicsBody2D
 
 func _ready():
-	paddle.material.set_shader_param("targ_color",color)
+	print(rotation_direction)
+	paddle.material.set_shader_param("targ_color", color)
 
 func _integrate_forces(_state):
-	input = int(Input.is_action_pressed(down)) - \
-			int(Input.is_action_pressed(up))
+	var input = int(Input.is_action_pressed(down)) - \
+				int(Input.is_action_pressed(up))
 	applied_force = Vector2(0, input * speed)
-	paddle.applied_torque = input * rotation_speed
+	paddle.applied_torque = rotation_direction * input * rotation_speed
