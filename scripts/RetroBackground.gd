d1f0d2b236fe106fabc5fc4d44e7a9c17001a04b
Leirda01 <adrieldj@orange.fr>
2020-04-19 20:55:50 +0200

    Score line smoothly move towards new value

--- scripts/RetroBackground.gd
+++ scripts/RetroBackground.gd
@@ -1,5 +1,12 @@
 extends CanvasLayer
 
 
+var target = 0
+
+
+func _process(delta):
+	$Background.position.x += (target - $Background.position.x) * 0.1
+
+
 func glide_to(coeff: float):
-	$Background.position.x = coeff * OS.get_window_size().x / 4
+	target = coeff * OS.get_window_size().x / 4
