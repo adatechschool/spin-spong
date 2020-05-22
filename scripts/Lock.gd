995de65eecab510f2d8d1997f761fe3bc053c970
Leirda01 <adrieldj@orange.fr>
2020-05-22 18:33:58 +0200

    Handle input each frame instead of physics frame

--- scripts/Lock.gd
+++ scripts/Lock.gd
@@ -9,14 +9,19 @@ export(Color) var color
 
 const speed:= 2000
 const rotation_speed:= 9000
+var input
 
 onready var paddle:= $Paddle as PhysicsBody2D
 
 func _ready():
 	paddle.material.set_shader_param("targ_color", color)
 
+
+func _process(_delta):
+	input = int(Input.is_action_pressed(down)) - \
+			int(Input.is_action_pressed(up))
+
+
 func _integrate_forces(_state):
-	var input = int(Input.is_action_pressed(down)) - \
-				int(Input.is_action_pressed(up))
-	applied_force = Vector2(0, input * speed)
-	paddle.applied_torque = rotation_direction * input * rotation_speed
+		applied_force = Vector2(0, input * speed)
+		paddle.applied_torque = rotation_direction * input * rotation_speed
