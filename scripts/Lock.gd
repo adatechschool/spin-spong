800ee777378e6bd5fa6379baab168826f76741d7
Leirda01 <adrieldj@orange.fr>
2020-05-23 18:43:07 +0200

    Fix non-existent variable at runtime!!

--- scripts/Lock.gd
+++ scripts/Lock.gd
@@ -7,7 +7,7 @@ export(String) var up
 export(String) var down
 export(Color) var color
 
-var input
+
 const speed:= 2000
 const rotation_speed:= 12000
 
@@ -17,11 +17,9 @@ func _ready():
 	paddle.material.set_shader_param("targ_color", color)
 
 
-func _process(_delta):
-	input = int(Input.is_action_pressed(down)) - \
-			int(Input.is_action_pressed(up))
-
-
 func _integrate_forces(_state):
-		applied_force = Vector2(0, input * speed)
-		paddle.applied_torque = rotation_direction * input * rotation_speed
+	var input = int(Input.is_action_pressed(down)) - \
+				int(Input.is_action_pressed(up))
+
+	applied_force = Vector2(0, input * speed)
+	paddle.applied_torque = rotation_direction * input * rotation_speed
