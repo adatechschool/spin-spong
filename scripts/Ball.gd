64fec011ff0e488f1130aed9a47cb95a3d8a48a4
Leirda01 <adrieldj@orange.fr>
2020-05-23 16:20:06 +0200

    Use normalized vector2 instead of speed value

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -27,7 +27,7 @@ func _integrate_forces(_state):
 	if linear_velocity.length() < min_speed:
 		linear_velocity = linear_velocity.normalized() * min_speed
 
-	if linear_velocity.dot(Vector2.UP) > min_speed * 0.8:
+	if linear_velocity.normalized().dot(Vector2.UP) > 0.8:
 		linear_velocity = Vector2.UP * min_speed
-	if linear_velocity.dot(Vector2.DOWN) > min_speed * 0.8:
+	if linear_velocity.normalized().dot(Vector2.DOWN) > 0.8:
 		linear_velocity = Vector2.DOWN * min_speed
