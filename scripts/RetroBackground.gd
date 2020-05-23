328fad728139b501753a9ecf48c9f012382a316d
Leirda01 <adrieldj@orange.fr>
2020-05-23 17:55:35 +0200

    Update background

--- scripts/RetroBackground.gd
+++ scripts/RetroBackground.gd
@@ -7,8 +7,9 @@ var target := Vector2.ZERO
 func _process(delta):
 	$Background.position = \
 		$Background.position.linear_interpolate(target, delta * speed)
+	$Gradient.position = - $Background.position
 
 
 func display_score(coeff: float):
-	target = Vector2(coeff * get_tree().root.size.x / 2, 0)
+	target = - Vector2(coeff * get_tree().root.size.x / 2, 0)
 
